e9caedb26c74e5f58476c416128546dac38fce30
salma-mindy <shellyaka998@gmail.com>
2020-09-05 19:10:08 +0000

    deploy my first flask app to heroku v3

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_word():
-    title = "Acceuil | J'apprend Flask"
+def home():
+    title = "Acceuil | Anoa"
     return render_template("home.html", title=title)
 
-@app.route('/details')
-def details():
-    title = "Details | J'apprend Flask"
+@app.route('/products')
+def products():
+    title = "Products | Anoa"
     return render_template("details.html", title=title)
+
+@app.route('/news')
+def news():
+    title = "News | Anoa"
+    return render_template("news.html", title=title)
+
+@app.route('/newsletter')
+def newsletter():
+    title = "Newsletter | Anoa"
+    return render_template("newsletter.html", title=title)
+
+@app.route('/contacts')
+def contacts():
+    title = "Contacts | Anoa"
+    return render_template("contacts.html", title=title)
