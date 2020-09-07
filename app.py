38e129ce36b102ecac37e1dd0717b969e3c00b4e
salma-mindy <shellyaka998@gmail.com>
2020-09-07 12:29:34 +0000

    flask v4

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    title = "Acceuil | Anoa"
+    title = "Acceuil | Anoa'Shopping"
     return render_template("home.html", title=title)
 
 @app.route('/products')
 def products():
-    title = "Products | Anoa"
+    title = "Products | Anoa'Shopping"
     return render_template("details.html", title=title)
 
 @app.route('/news')
 def news():
-    title = "News | Anoa"
+    title = "News | Anoa'Shopping"
     return render_template("news.html", title=title)
 
-@app.route('/newsletter')
+@app.route('/compte')
 def newsletter():
-    title = "Newsletter | Anoa"
+    title = "Newsletter | Anoa'Shopping"
     return render_template("newsletter.html", title=title)
 
 @app.route('/contacts')
 def contacts():
-    title = "Contacts | Anoa"
+    title = "Contacts | Anoa'Shopping"
     return render_template("contacts.html", title=title)
